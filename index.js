93e9a2d6c1d588ed7e9591765897b301798f8be8
alegria <751510087@qq.com>
2018-08-15 19:21:34 +0800

    explore mongo and mysql

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var MongoDB = require('./lib/mongo/MongoDB');
 var MongoDao = require('./lib/mongo/MongoDao');
+var Mongo = require('./lib/mongo/Mongo');
 var MysqlDB = require('./lib/mysql/MysqlDB');
 var MysqlDao = require('./lib/mysql/MysqlDao');
+var Mysql = require('./lib/mysql/Mysql');
 var api = require('./lib/apiResult');
 var Redis = require('./lib/Redis');
 var errors = require('./lib/errors');
@@ -21,7 +23,9 @@ var enuo = {
     errors: errors,
     logger: logger,
     utils: utils,
+    Mongo: Mongo,
     MongoDB: MongoDB,
+    Mysql: Mysql,
     MysqlDB: MysqlDB,
     Redis: Redis,
     MongoDao: MongoDao,
